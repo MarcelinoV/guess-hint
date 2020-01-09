04cb82d016c7f1552b294601a2860c26ee346c49
Marcelino Velasquez <marcelinomv98x@gmail.com>
2020-01-09 18:53:14 -0500

    Even cleaner code and proper credits
    The comments have been moved around to make more sense and I've given credit to freecodecamp.org which helped me immensely on my journey in learning python.

--- guess_w_hints.py
+++ guess_w_hints.py
@@ -2,39 +2,49 @@
 # Modules used
 import random 
 
-word_bank = [{ # nested dictionary within a list
+# This first part of the code randomly selects a word & its hint (key:value)
+# from the word_bank, which is a dictionary. It then prints the hint and intro
+# message
+
+word_bank = { # dictionary, the keys are the secret words & the values are the hints
 'queen': 'She runs the game',
 'rook': "home sweet home",
 'knight': 'Shining armor',
 'king': "Doesn't do much like modern ones",
 'bishop':'Religious zealot',
-'pawn':'Another cog in the system'}]
+'pawn':'Another cog in the system'}
 
 bank_keys = [] # To be appended list
+
+for key in word_bank.keys(): # dict is index 0 in list, method returns keys of dict
+    bank_keys.append(key) # append keys to list
+       
+secret_word = random.choice(bank_keys) # uses function to randomly choose word
+hint = word_bank[secret_word] # ensures correct hint is assigned to secret word
+
+print("Guess which chess piece this is!\n")
+
+print('Hint: ' + hint) # prints hint
+
+# This second part of the code creates the game itself and its functions,
+# including a guess limit and guess count
+
 guess = "" # empty string
 guess_count = 0 # # of guesses user has made
 guess_limit = 3 # # of guesses user can make
 out_of_guesses = False # initial condition, User hasn't guessed yet
 
-for dict in word_bank: # access dict inside word_bank which is a list
-    for key in word_bank[0].keys(): # dict is index 0 in list, method returns keys of dict
-        bank_keys.append(key) # append keys to list
-       
-secret_word = random.choice(bank_keys) # uses function to randomly choose word
-hint = word_bank[0][secret_word] # ensures correct hint is assigned to secret word
-
-print(hint) # prints hint
-
 # and or & not are logical operators used to set conditions
 # not is used to say that the condition has to be false
 # for code to run, for example:
-    
+
 while guess != secret_word and not(out_of_guesses): # conditions that need to be met for game to end
 
 # initial variable values are disregarded in logical statements
 # while & if assume everything after IS TRUE
+    
     if guess_count < guess_limit: # While user hasn't surpassed guess limit
-        guess = input("Enter guess: ") # input value for guess variable
+        guess = input("\nEnter guess: ") # input value for guess variable
         guess_count += 1 # increment of guess count per guess
     else:
         out_of_guesses = True # becomes true if if clause above becomes untrue
@@ -43,6 +53,9 @@ if out_of_guesses:
     print("Out of guesses, you LOSE!") # Losing message
 
 else:
-    print("Nice guess, you WIN!") # Winning message
+    print("Nice guess, you WIN!\n") # Winning message
 
 print(secret_word)
+
+# I originally learned the guessing game when I was first learning python with freeCodeCamp on Youtube,
+# but here I've expanded on it with the word bank code. Credits to freeCodeCamp for the inspiration.
